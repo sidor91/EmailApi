9c2e99d885ca9c7c3c33af7f1879da9f9f611258
Serhii Sydorenko <sidorchik91@gmail.com>
2024-04-05 18:38:41 +0200

    ADD | attachment to email

--- src/services/emailService.ts
+++ src/services/emailService.ts
@@ -1,28 +1,16 @@
 require('dotenv').config();
 import nodemailer from 'nodemailer';
+import path from 'path';
+import fs from 'fs'
 import * as configs from './configs';
 import * as t from './types';
 import { generateEmailLayout } from '../utils/generateEmailLayout';
 
-const waveResponseExample = {
-  status: { success: true, httpstatuscode: 200 },
-  statistics: {
-    pagetitle: 'Ecomitize: We Grow eCommerce Websites and Businesses',
-    pageurl: 'https://ecomitize.com/',
-    time: 4.32,
-    creditsremaining: 95,
-    allitemcount: 195,
-    totalelements: 674,
-    waveurl: 'http://wave.webaim.org/report?url=https://ecomitize.com/',
-  },
-  categories: {
-    error: { description: 'Errors', count: 14 },
-    contrast: { description: 'Contrast Errors', count: 20 },
-    alert: { description: 'Alerts', count: 7 },
-    feature: { description: 'Features', count: 42 },
-    structure: { description: 'Structural Elements', count: 51 },
-    aria: { description: 'ARIA', count: 61 },
-  },
+const pdfFilePath = path.join('assets', 'Website Accessibility Guide.pdf');
+const attachment = {
+  filename: 'Website Accessibility Guide.pdf',
+  content: fs.readFileSync(pdfFilePath),
+  contentType: 'application/pdf',
 };
 
 class EmailService {
@@ -40,6 +28,7 @@ class EmailService {
       subject: `Web Accessibility Test Results for ${statistics.pageurl}`,
       text: 'Hello world?',
       html: generateEmailLayout(data),
+      attachments: [attachment],
     });
   }
 }
--- src/utils/generateEmailLayout.ts
+++ src/utils/generateEmailLayout.ts
@@ -1,30 +1,8 @@
-import { WaveResponse } from '../services/types'
-
-const waveResponseExample = {
-  status: { success: true, httpstatuscode: 200 },
-  statistics: {
-    pagetitle: 'Ecomitize: We Grow eCommerce Websites and Businesses',
-    pageurl: 'https://ecomitize.com/',
-    time: 4.32,
-    creditsremaining: 95,
-    allitemcount: 195,
-    totalelements: 674,
-    waveurl: 'http://wave.webaim.org/report?url=https://ecomitize.com/',
-  },
-  categories: {
-    error: { description: 'Errors', count: 14 },
-    contrast: { description: 'Contrast Errors', count: 20 },
-    alert: { description: 'Alerts', count: 7 },
-    feature: { description: 'Features', count: 42 },
-    structure: { description: 'Structural Elements', count: 51 },
-    aria: { description: 'ARIA', count: 61 },
-  },
-};
-
+import { WaveResponse } from '../services/types';
 
 export const generateEmailLayout = (data: WaveResponse) => {
   const { statistics, categories } = data;
-  
+
   return `<html lang="en">
 <head>
     <style>
@@ -238,4 +216,4 @@ export const generateEmailLayout = (data: WaveResponse) => {
 </div>
 </body>
 </html>`;
-}
+};
